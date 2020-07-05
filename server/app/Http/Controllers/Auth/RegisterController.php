dcbf1c24d4c013dded957167d77ae4961ffb254e
Masaharu Inoue <pasteur1822@gmail.com>
2020-07-05 23:06:43 +0900

    feat: 新規登録時にユーザーレベルも追加

--- server/app/Http/Controllers/Auth/RegisterController.php
+++ server/app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use App\UserLevel;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -44,7 +45,7 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -62,15 +63,27 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param array $data
      * @return \App\User
      */
     protected function create(array $data)
     {
-        return User::create([
+        $lastUser = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        $lastId = $lastUser->id;
+
+        $userLevel = new UserLevel();
+        $userLevel->id = $lastId;
+        $userLevel->level = 1;
+        $userLevel->times = "";
+        $isSuccess = $userLevel->save();
+        if (!$isSuccess) {
+            throw new \Exception("ユーザーレベルの追加に失敗しました");
+        }
+
+        return $lastUser;
     }
 }
