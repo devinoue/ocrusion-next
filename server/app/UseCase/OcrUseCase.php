6572004de844009d778febcf3c8218da9ab8baaf
Masaharu Inoue <pasteur1822@gmail.com>
2020-07-10 01:35:03 +0900

    fix: インデント変更

--- server/app/UseCase/OcrUseCase.php
+++ server/app/UseCase/OcrUseCase.php
@@ -15,55 +15,55 @@ use App\Repository\DB\OcrTextRepository;
 
 final class OcrUseCase
 {
-  private $imgDirRepository;
-  private $ocrTextRepository;
-  private $stateService;
-  private $cloudVision;
+    private $imgDirRepository;
+    private $ocrTextRepository;
+    private $stateService;
+    private $cloudVision;
 
-  function execute()
-  {
-    $imgDirRepository = new ImgDirRepository();
-    $ocrTextRepository = new OcrTextRepository();
-    $stateService = new StateService();
-    $cloudVision = new CloudVision();
+    function execute()
+    {
+        $imgDirRepository = new ImgDirRepository();
+        $ocrTextRepository = new OcrTextRepository();
+        $stateService = new StateService();
+        $cloudVision = new CloudVision();
 
-    if ($stateService->isExecuting()) {
-      return;
+        if ($stateService->isExecuting()) {
+            return;
+        }
+
+        $error = $stateService->getError();
+        if ($error) {
+            ErrorLogRepository::save($error, 0, $stateService->getBookId());
+        }
+
+
+        $bookId = new BookId($stateService->getBookId());
+        $userId = new UserId($stateService->getUserId());
+
+        $stateService->lock();
+
+
+        try {
+            $ocr_texts = $cloudVision->read($userId, $bookId);
+            $ocrTextRepository->saveAll($ocr_texts);
+
+            $stateService->clear($bookId);
+
+            $imgDir = $imgDirRepository->find($bookId);
+            $imgDir->changeStateDone();
+            $imgDirRepository->updateState($imgDir);
+
+            return ["message" => "success"];
+        } catch (\Exception $e) {
+            ErrorLogRepository::save($e->getMessage(), $userId->value(), $bookId->value());
+
+            $stateService->exceptionUnlock();
+
+            $imgDir = $imgDirRepository->find($bookId);
+            $imgDir->changeStateException();
+            $imgDirRepository->updateState($imgDir);
+
+            return ["error" => $e->getMessage()];
+        }
     }
-
-    $error = $stateService->getError();
-    if ($error) {
-      ErrorLogRepository::save($error, 0, $stateService->getBookId());
-    }
-
-
-    $bookId = new BookId($stateService->getBookId());
-    $userId = new UserId($stateService->getUserId());
-
-    $stateService->lock();
-
-
-    try {
-      $ocr_texts = $cloudVision->read($userId, $bookId);
-      $ocrTextRepository->saveAll($ocr_texts);
-
-      $stateService->clear($bookId);
-
-      $imgDir = $imgDirRepository->find($bookId);
-      $imgDir->changeStateDone();
-      $imgDirRepository->updateState($imgDir);
-
-      return ["message" => "success"];
-    } catch (\Exception $e) {
-      ErrorLogRepository::save($e->getMessage(), $userId->value(), $bookId->value());
-
-      $stateService->exceptionUnlock();
-
-      $imgDir = $imgDirRepository->find($bookId);
-      $imgDir->changeStateException();
-      $imgDirRepository->updateState($imgDir);
-
-      return ["error" => $e->getMessage()];
-    }
-  }
 }
