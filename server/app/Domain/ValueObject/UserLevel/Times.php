80dd37d96e52e0fb1f566daa3b784031e008cd94
Masaharu Inoue <pasteur1822@gmail.com>
2020-07-09 20:35:37 +0900

    add: joinedTimesを追加

--- server/app/Domain/ValueObject/UserLevel/Times.php
+++ server/app/Domain/ValueObject/UserLevel/Times.php
@@ -6,46 +6,57 @@ use App\Domain\ValueObject\BaseValueObject;
 
 final class Times implements BaseValueObject
 {
-  public $value;
+    public $value;
 
-  public function __construct(array $primitive)
-  {
-    $this->validate($primitive);
-    $this->value = $primitive;
-  }
-
-  public static function create(array $primitive): Times
-  {
-    $instance = new Times($primitive);
-    return $instance;
-  }
-
-  private function validate($primitive)
-  {
-    // validation
-  }
-
-  public function setTimes(array $times)
-  {
-    $this->value = $times;
-  }
-
-  public static function splitTimes(string $stringTimes): array
-  {
-    $explodedTimes = explode("/", $stringTimes);
-
-    $sizeTimeSet = [];
-    if (!isset($explodedTimes[0])) return $sizeTimeSet;
-    foreach ($explodedTimes as $value) {
-      $explodedValue = explode("-", $value);
-      if (!isset($explodedValue[1])) continue;
-      $sizeTimeSet[$explodedValue[0]] = $explodedValue[1];
+    public function __construct(array $primitive)
+    {
+        $this->validate($primitive);
+        $this->value = $primitive;
     }
-    return $sizeTimeSet;
-  }
 
-  public function value(): array
-  {
-    return $this->value;
-  }
+    public static function create(array $primitive): Times
+    {
+        $instance = new Times($primitive);
+        return $instance;
+    }
+
+    private function validate($primitive)
+    {
+        // validation
+    }
+
+    public function setTimes(array $times)
+    {
+        $this->value = $times;
+    }
+
+    public static function splitTimes(string $stringTimes): array
+    {
+        $explodedTimes = explode("/", $stringTimes);
+
+        $sizeTimeSet = [];
+        if (!isset($explodedTimes[0])) return $sizeTimeSet;
+        foreach ($explodedTimes as $value) {
+            $explodedValue = explode("-", $value);
+            if (!isset($explodedValue[1])) continue;
+            $sizeTimeSet[$explodedValue[0]] = $explodedValue[1];
+        }
+        return $sizeTimeSet;
+    }
+
+    public function joinTimes(): string
+    {
+        $sizeTimeSet = $this->value;
+        $stringTimes = "";
+        foreach ($sizeTimeSet as $key => $value) {
+            $stringTimes .= $key . "-" . $value;
+            $stringTimes .= "/";
+        }
+        return $stringTimes;
+    }
+
+    public function value(): array
+    {
+        return $this->value;
+    }
 }
