f27b93bc92fd899077b9a9ed833fcf4a6eaf594e
Masaharu Inoue <pasteur1822@gmail.com>
2020-07-28 02:13:46 +0900

    fix: デザイン適用

--- client/pages/members/upload.vue
+++ client/pages/members/upload.vue
@@ -7,7 +7,7 @@
     />
     <AppLeading title="UPLOAD" sub-title="アップロード" class="text-center" />
     <UploadCapacity :capacity="capacity" class="text-center" />
-    <UploadForm :status="status" @uploadParams="uploadParams" />
+    <UploadForm @uploadParams="uploadParams" />
 
     <button large @click.stop="batch()">
       バッチ実行
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { ref, onMounted } from '@vue/composition-api'
+import { ref, onMounted, SetupContext } from '@vue/composition-api'
 import axios from 'axios'
 import { RequestState } from '~/types'
 import useLoading from '~/composables/use-loading'
@@ -24,14 +24,14 @@ import useLoading from '~/composables/use-loading'
 export default {
   name: 'Upload',
   layout: 'member',
-  setup(_props: {}) {
+  setup(_props: {}, { root }: SetupContext) {
+    const userId = root.$store.getters['Auth/user'].id
+    // const userId = '1222333322233'
     const { changeLoaded, changeLoading, changeFailure, request } = useLoading()
     const capacity = ref(0)
     onMounted(async () => {
       try {
-        const res2 = await axios.get(
-          'http://localhost:8080/api/capacities/3OfY3rPywDtU749NjsuynhiyOS9mjbRZPw4i'
-        )
+        const res2 = await axios.get(`/api/capacities/${userId}`)
         capacity.value = res2.data.capacity
       } catch (e) {
         console.log(e.response)
@@ -40,22 +40,20 @@ export default {
     const batch = async () => {
       const url = `/api/batch`
       try {
-        const baseApi = axios.create({ baseURL: 'http://localhost:8080' })
-        const res = await baseApi.get(url)
+        const res = await axios.get(url)
         console.log(res)
       } catch (e) {
         console.log(e.response)
       }
     }
-    const status = ref(RequestState.UNINITIALIZED)
+
     const uploadParams = async (params: any) => {
       changeLoading()
-      const url = `/api/files/${params.userId}`
+      const url = `/api/files/${userId}`
       console.log(url)
       const headers = { 'content-type': `multipart/form-data` }
       try {
-        const baseApi = axios.create({ baseURL: 'http://localhost:8080' })
-        const res = await baseApi.post(url, params, {
+        const res = await axios.post(url, params, {
           headers,
         })
         console.log(res)
@@ -70,7 +68,6 @@ export default {
     return {
       batch,
       uploadParams,
-      status,
       changeLoaded,
       capacity,
       request,
