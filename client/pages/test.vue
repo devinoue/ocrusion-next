2b4e3f98d3dfd94f2419dca03f558e6d652d1da2
Masaharu Inoue <pasteur1822@gmail.com>
2020-07-28 02:14:21 +0900

    feat: 試験用ページ追加

---
+++ client/pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>a : {{ g }}</div>
+</template>
+<script lang="ts">
+import { ref, SetupContext } from 'nuxt-composition-api'
+export default {
+  name: '',
+  layout: 'member',
+  setup(_props: {}, { root }: SetupContext) {
+    console.log(root.$store.getters['Auth/token'])
+    const g = root.$store.getters['Auth/token']
+    return { g }
+  },
+}
+</script>
+<style lang="scss" scoped></style>
