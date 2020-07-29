040e72402864bdc2b795ba58ba96247f7776cb26
Masaharu Inoue <pasteur1822@gmail.com>
2020-07-30 01:33:31 +0900

    feat: 動的なタイトル変更の追加

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -11,7 +11,8 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+    titleTemplate: '自炊OCRアプリ | %s',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
