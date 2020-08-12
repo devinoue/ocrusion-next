4e4f49e8f1fc0fb570b66eff013422451dd95587
Masaharu Inoue <pasteur1822@gmail.com>
2020-08-12 20:18:31 +0900

    fix: htmlタグにlang属性追加

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -12,6 +12,9 @@ export default {
    */
   head: {
     titleTemplate: '%s | 自炊OCRアプリ',
+    htmlAttrs: {
+      lang: 'ja',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
